c78c788115688e6712ad317bfe024f4f93980379
Mounix99 <sience1999@gmail.com>
2022-12-14 00:05:17 +0100

    update

--- lib/prezentation/profile_page/profile_page.dart
+++ lib/prezentation/profile_page/profile_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:image_picker_for_web/image_picker_for_web.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 import 'package:salons_adminka/injection_container_web.dart';
 import 'package:salons_adminka/prezentation/profile_page/profile_bloc.dart';
 import 'package:salons_adminka/prezentation/widgets/custom_app_bar.dart';
@@ -50,7 +51,7 @@ class _ProfilePageState extends State<ProfilePage> {
     LocalStorage localStorage = getIt<LocalStorage>();
     String? salonId = localStorage.getSalonId();
 
-    print("salonId: $salonId");
+    debugPrint("salonId: $salonId");
 
     _profileBloc = getItWeb<ProfileBloc>();
     _profileBloc.loadSalon(salonId ?? "");
@@ -81,34 +82,140 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(left: 42, right: 50, bottom: 35),
-        child: CustomScrollView(
-          slivers: [
-            SliverFillRemaining(
-              hasScrollBody: false,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  CustomAppBar(title: AppLocalizations.of(context)!.profileSettings),
-                  Expanded(
-                    child: StreamBuilder<Salon>(
-                      stream: _profileBloc.salonLoaded,
-                      builder: (context, snapshot) {
-                        if (snapshot.data != null) {
-                          return _buildSalonDetails(snapshot.data!);
-                        }
-                        return const Center(
-                          child: CircularProgressIndicator(),
-                        );
-                      },
-                    ),
+      body: StreamBuilder<Salon>(
+          stream: _profileBloc.salonLoaded,
+          builder: (context, snapshot) {
+            return ScreenTypeLayout.builder(
+              desktop: (context) => _buildDesktopContent(context, snapshot),
+              tablet: (context) => _buildMobileContent(context, snapshot),
+              mobile: (context) => _buildMobileContent(context, snapshot),
+            );
+          }),
+    );
+  }
+
+  Widget _buildMobileContent(BuildContext context, AsyncSnapshot<Salon> snapshot) {
+    final salon = snapshot.data;
+    return Center(
+      child: FractionallySizedBox(
+        widthFactor: 0.8,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CustomAppBar(title: AppLocalizations.of(context)!.profileSettings),
+            SizedBox(
+              width: 640,
+              height: 182,
+              child:
+                  _pickedPhoto != null ? _buildPhotoWidget(_pickedPhoto!.path) : _buildPhotoWidget(salon?.photo ?? ""),
+            ),
+            const SizedBox(height: 16),
+            snapshot.data != null
+                ? Flexible(child: SingleChildScrollView(child: _mobileSalonDetails(context, snapshot.data!)))
+                : const Center(
+                    child: CircularProgressIndicator(),
                   ),
-                ],
+            Material(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: RoundedButton(
+                  width: double.infinity,
+                  text: AppLocalizations.of(context)!.saveChanges,
+                  onPressed: () {
+                    final salon = snapshot.data!;
+                    _errorText = "";
+
+                    if (_nameFormKey.currentState?.validate() == true &&
+                        _descriptionFormKey.currentState?.validate() == true &&
+                        _addressFormKey.currentState?.validate() == true &&
+                        _phoneFormKey.currentState?.validate() == true) {
+                      salon.name = _nameController.text;
+                      salon.description = _descriptionController.text;
+                      salon.address = _addressController.text;
+                      salon.phoneNumber = _phoneController.text;
+
+                      _profileBloc.updateSalon(salon, _pickedPhoto);
+                    }
+                  },
+                ),
               ),
             )
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _mobileSalonDetails(BuildContext context, Salon salon) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            _buildSettingsTitle(AppLocalizations.of(context)!.photo),
+            const Spacer(),
+            IconButton(
+              splashRadius: 18,
+              onPressed: () async {
+                final PickedFile image = await _picker.pickImage(source: ImageSource.gallery);
+                setState(() {
+                  _pickedPhoto = image;
+                });
+              },
+              icon: SvgPicture.asset(AppIcons.icEditCircle),
+            ),
+            // const SizedBox(width: 8),
+            IconButton(
+              splashRadius: 18,
+              onPressed: () {
+                setState(() {
+                  if (_pickedPhoto != null) {
+                    _pickedPhoto = null;
+                  } else if (salon.photo != null) {
+                    salon.photo = null;
+                  }
+                });
+              },
+              icon: SvgPicture.asset(AppIcons.icDeleteCircle),
+            ),
+          ],
+        ),
+        const SizedBox(height: 24),
+        _buildSettingsRow(AppLocalizations.of(context)!.salonName, salon.name, _nameController, _nameFormKey),
+        const SizedBox(height: 15),
+        _buildSettingsRow(
+            AppLocalizations.of(context)!.description, salon.description, _descriptionController, _descriptionFormKey,
+            hint: AppLocalizations.of(context)!.shortSalonDescription),
+        const SizedBox(height: 15),
+        _buildSettingsRow(AppLocalizations.of(context)!.address, salon.address, _addressController, _addressFormKey),
+        const SizedBox(height: 15),
+        _buildSettingsRow(
+            AppLocalizations.of(context)!.phoneNumber, salon.phoneNumber, _phoneController, _phoneFormKey),
+        // _buildSettingsRow("График работы", null, _scheduleController, _scheduleFormKey),
+      ],
+    );
+  }
+
+  Widget _buildDesktopContent(BuildContext context, AsyncSnapshot<Salon> snapshot) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 42, right: 50, bottom: 35),
+      child: CustomScrollView(
+        slivers: [
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CustomAppBar(title: AppLocalizations.of(context)!.profileSettings),
+                Expanded(
+                    child: snapshot.data != null
+                        ? _buildSalonDetails(snapshot.data!)
+                        : const Center(
+                            child: CircularProgressIndicator(),
+                          ))
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
@@ -257,7 +364,7 @@ class _ProfilePageState extends State<ProfilePage> {
               textAlignVertical: TextAlignVertical.center,
               readOnly: formKey == _scheduleFormKey,
               onTap: () {
-                print("on tap");
+                debugPrint("on tap");
               },
               validator: (text) {
                 if (_errorText == null) {
@@ -273,12 +380,12 @@ class _ProfilePageState extends State<ProfilePage> {
                   return AppLocalizations.of(context)!.fieldMustContainMoreThanCharacters;
                 }
                 if (formKey == _phoneFormKey) {
-                  if (!RegExp(r"^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$").hasMatch(text)) {
+                  if (!RegExp(r"^[+]?[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{4,6}$").hasMatch(text)) {
                     return AppLocalizations.of(context)!.enterPhoneInRightForm;
                   }
                 }
                 if (formKey == _addressFormKey) {
-                  if (!RegExp(r"(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+").hasMatch(text)) {
+                  if (!RegExp(r"(?:(?:https?|ftp)://)?[\w/\-?=%.]+\.[\w/\-?=%.]+").hasMatch(text)) {
                     return AppLocalizations.of(context)!.enterGoogleMapLink;
                   }
                 }
